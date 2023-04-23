4c700b47764b506206ad76e7684146c1cd674c4b
Tai1013 <frank22199215@gmail.com>
2023-04-24 01:29:33 +0800

    refactor: tailwind config delete warn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 import colors from 'tailwindcss/colors'
 
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
 export default {
   mode: 'jit',
   darkMode: 'class',
